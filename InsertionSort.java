d5f8f11cf7b99018d55a56e0b8a80ff5351994d7
brian-mokaya <brianmokaya507@gmail.com>
2025-08-31 08:25:50 +0300

    java IS

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,6 +1,23 @@
 public class InsertionSort {
     public static void main(String[] args) {
-        
+        int arr[] = {5,6,8,3,1,6};
+
+        for(int i=1; i<arr.length; i++){
+            int temp = arr[i];
+            int j = i-1;
+
+            while(j>=0 && arr[j]>temp){
+
+                arr[j+1] = arr[j];
+                j--;
+            }
+
+            arr[j+1] = temp;
+
+        }
+        for (int num : arr) {
+            System.out.print(num + " ");            
+        }
     }
     
 }
